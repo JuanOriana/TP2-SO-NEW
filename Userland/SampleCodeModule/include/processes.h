48cbad0cf7c0436a0ba298e68653665a324fcafb
Juan Oriana <joriana@itba.edu.ar>
2021-04-27 17:17:16 -0300

    Casting warn addressed

--- Userland/SampleCodeModule/include/processes.h
+++ Userland/SampleCodeModule/include/processes.h
@@ -4,10 +4,9 @@
 #include <stdint.h>
 #include <systemCalls.h>
 
-int createProcess(void (*entryPoint)(int, char **), int argc, char **argv);
+int createProcess(void (*entryPoint)(int, const char **), int argc, char **argv);
 int killProcess(uint64_t pid);
 int blockProcess(uint64_t pid);
 int unlockProcess(uint64_t pid);
-
 
 #endif
