99725f99986791c1fefd50575c30370996ec82f0
Juan Oriana <joriana@itba.edu.ar>
2021-05-09 21:32:14 -0300

    cant kill shell

--- Userland/SampleCodeModule/test_process/test_processes.c
+++ Userland/SampleCodeModule/test_process/test_processes.c
@@ -16,7 +16,7 @@ void process1(int argc, char *argv[])
 {
     while (1)
     {
-        printStringLn(argv[1]);
+        printString(argv[1]);
     }
 }
 
@@ -51,9 +51,9 @@ void test_processes()
         for (rq = 0; rq < MAX_PROCESSES; rq++)
         {
             char processName[30];
-            strcopy("Proceso Dummy",processName);
+            strcopy("Proceso Dummy", processName);
             char *argv[] = {processName, itoa(rq, buf, 10)};
-            p_rqs[rq].pid = createProcess(&process1, 2, argv,1); //TODO: Port this call as required
+            p_rqs[rq].pid = createProcess(&process1, 2, argv, 1); //TODO: Port this call as required
 
             if (p_rqs[rq].pid == -1)
             {                                            //TODO: Port this as required
